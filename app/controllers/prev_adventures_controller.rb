2fd45a6ca3eebb4f46264203469ee88676d62254
Ethan Schiavi <etschiavi@gmail.com>
2015-09-02 22:50:32 -0400

    commit

--- app/controllers/prev_adventures_controller.rb
+++ app/controllers/prev_adventures_controller.rb
@@ -7,8 +7,7 @@ class PrevAdventuresController < ApplicationController
 		# @prev_adven = Prev_Adventure.where(user_id: @current_user.id)
 		if logged_in? && check_current_user? 
 			@current_user = User.find(session[:user_id])
-			# @prev_adven = Prev_Adventure.find(session[:user_id])
-		# binding.pry
+
 		else
 			# redirect_to user_path(@current_user)
 		end
